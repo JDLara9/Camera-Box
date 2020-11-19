c88eb9ed2a94e1d1fa27913386b3187df3c0ed11
Joshua Lara <Joshualara9@gmail.com>
2020-11-18 20:10:17 -0600

    Initial Commit

---
+++ Camera Box/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera Box
+//
+//  Created by Joshua Lara on 11/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let button = UIButton()
+    }
+
+
+}
+
